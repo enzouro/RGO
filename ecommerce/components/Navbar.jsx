da44f5155bb549a1e77190f64728f7697edba924
John <lorenmayo15@gmail.com>
2023-11-13 01:22:20 +0800

    remove some useless functions

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import Link from 'next/link';
-import { AiOutlineShopping } from 'react-icons/ai'
 
-import { Cart } from './';
-import { useStateContext} from '../context/StateContext';
 
 const Navbar = () => {
-  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
 
   return (
     <div className="navbar-container">
@@ -17,13 +14,6 @@ const Navbar = () => {
         <p className="logo">
         <Link href="/"><br/>Resource Generation Office - Lipa</Link>
         </p>
-  
-      {/*<button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
-        <AiOutlineShopping />
-        <span className="cart-item-qty">{totalQuantities}</span>
-        </button> */}
-
-      {showCart && <Cart />} 
     </div>
   )
 }
